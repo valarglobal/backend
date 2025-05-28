3b28227e9b081faa705c2c062a68b1ad3b0a3d5a
Abukstech <abdulmajeedbamidele4@gmail.com>
2025-05-28 01:16:35 -0700

    feat:add production server

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
   .setVersion('1.0')
   .addBearerAuth()
   .addServer('http://localhost:3000', 'Local environment')
+  .addServer('valar-pay-api.up.railway.app', 'Production environment') 
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api/docs', app, document);
